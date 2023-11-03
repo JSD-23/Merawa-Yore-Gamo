cd4b6085e34ee79cf069505a982be1f880b85965
Merawa Yore Gamo <dundeemerawa@gmail.com>
2023-11-03 10:01:33 +0100

    Update Exercice2.cs

--- Exercice2.cs
+++ Exercice2.cs
@@ -1,3 +1,26 @@
-int MaxProfit(int[] prix, int prix_size) {
-    // Votre code ici
+using System;
+
+class Exercice2
+{
+	public int MaxProfit(int[] prix, int prix_size) 
+	{
+	
+		int max = 0;
+		
+		if (prix_size == 0)
+			return  0;
+		
+		for (int i = 0; i < prix_size - 1; i++)
+		{
+			for (int j = i + 1; j < prix_size; j++)
+			{
+				if ((prix[i] - prix[j]) > max)
+				{
+					max = prix[i] - prix[j];
+				}
+			}
+		}
+		
+		return max;
+	}
 }
