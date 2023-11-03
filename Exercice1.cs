596bda75583ccc54c465dac4d23f109304ff2ec2
Merawa Yore Gamo <dundeemerawa@gmail.com>
2023-11-03 09:29:56 +0100

    Update Exercice1.cs

--- Exercice1.cs
+++ Exercice1.cs
@@ -1,4 +1,21 @@
-public int LengthOfLastWord(string s) {
-    // Votre code ici
-    return 0;
+using System;
+
+class Exercice1
+{
+	public int LengthOfLastWord(string s) 
+	{
+		if (0 == s.Length)
+			return 0;
+		
+		int  lastIndex = s.LastIndexOf(" ");
+		
+		if (-1 == lastIndex)
+			return s.Length;
+		
+		if ( lastIndex != s.Length -1)
+			return s.Substring(lastIndex+1).Length;
+		
+		return s.Length - 1;
+	}
+    
 }
